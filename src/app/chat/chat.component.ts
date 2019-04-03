9d99be96a0031a86717623d8cfeb8d02f4daf061
matteoxx <mateuszniziolek96@gmail.com>
2019-04-03 16:17:11 +0200

    web socket without auth

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component} from '@angular/core';
 import * as Stomp from 'stompjs';
 import * as SockJS from 'sockjs-client';
 
@@ -7,67 +7,150 @@ import * as SockJS from 'sockjs-client';
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit, OnDestroy{
+export class ChatComponent{
 
-  ngOnInit(){
-    this.connect();
+  // usernamePage = document.querySelector('#username-page');
+  // chatPage = document.querySelector('#chat-page');
+  usernameForm = document.querySelector('#usernameForm');
+  messageForm = document.querySelector('#messageForm');
+  messageInput = document.querySelector<HTMLInputElement>('#message');
+  messageArea = document.querySelector('#messageArea');
+  connectingElement = document.querySelector('.connecting');
+
+  stompClient = null;
+  username = null;
+
+  colors = [
+    '#2196F3', '#32c787', '#00BCD4', '#ff5652',
+    '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
+  ];
+
+
+
+  onError(error: Error) {
+    document.querySelector<HTMLDivElement>('.connecting').textContent = 'Could not connect to WebSocket server. Please refresh this page to try again!';
+    document.querySelector<HTMLDivElement>('.connecting').style.color = 'red';
   }
 
-  ngOnDestroy(){
-    this.disconnect();
+
+  sendMessage(event: Event) {
+      var messageContent = document.querySelector<HTMLInputElement>('#message').value.trim();
+      if(messageContent && this.stompClient) {
+          var chatMessage = {
+              sender: this.username,
+              content: document.querySelector<HTMLInputElement>('#message').value,
+              type: 'CHAT'
+          };
+          this.stompClient.send("/app/chat.sendMessage", {}, JSON.stringify(chatMessage));
+          document.querySelector<HTMLInputElement>('#message').value = '';
+      }
+      event.preventDefault();
   }
- 
-  greetings: string[] = [];
-  disabled = true;
-  name: string;
-  private stompClient = null;
- 
-  constructor() { }
- 
-  setConnected(connected: boolean) {
-    this.disabled = !connected;
- 
-    if (connected) {
-      this.greetings = [];
+
+
+  onMessageReceived(payload) {
+    var message = JSON.parse(payload.body);
+
+    var messageElement = document.createElement('li');
+
+    if(message.type === 'JOIN') {
+        messageElement.classList.add('event-message');
+        message.content = message.sender + ' joined!';
+    } else if (message.type === 'LEAVE') {
+        messageElement.classList.add('event-message');
+        message.content = message.sender + ' left!';
+    } else {
+        messageElement.classList.add('chat-message');
+
+        var avatarElement = document.createElement('i');
+        var avatarText = document.createTextNode(message.sender[0]);
+        avatarElement.appendChild(avatarText);
+        avatarElement.style['background-color'] = this.getAvatarColor(message.sender);
+
+        messageElement.appendChild(avatarElement);
+
+        var usernameElement = document.createElement('span');
+        var usernameText = document.createTextNode(message.sender);
+        usernameElement.appendChild(usernameText);
+        messageElement.appendChild(usernameElement);
+    }
+
+    var textElement = document.createElement('p');
+    var messageText = document.createTextNode(message.content);
+    textElement.appendChild(messageText);
+
+    messageElement.appendChild(textElement);
+
+    this.messageArea.appendChild(messageElement);
+    this.messageArea.scrollTop = this.messageArea.scrollHeight;
+  }
+
+  getAvatarColor(messageSender) {
+    var hash = 0;
+    for (var i = 0; i < messageSender.length; i++) {
+        hash = 31 * hash + messageSender.charCodeAt(i);
+    }
+    var index = Math.abs(hash % this.colors.length);
+    return this.colors[index];
+  }
+
+  connect(event: Event) {
+    this.username = document.querySelector<HTMLInputElement>('#name').value.trim();
+
+    if(this.username) {
+        document.querySelector<HTMLDivElement>('#username-page').classList.add('hidden');
+        document.querySelector<HTMLDivElement>('#chat-page').classList.remove('hidden');
+
+        const _this = this;
+        var socket = new SockJS('https://applicationfitness.herokuapp.com/ws');
+        this.stompClient = Stomp.over(socket);
+        this.stompClient.connect({}, function(frame){
+        
+          _this.stompClient.subscribe('/topic/public', function (payload) {
+
+            var message = JSON.parse(payload.body);
+
+            var messageElement = document.createElement('li');
+
+            if(message.type === 'JOIN') {
+                messageElement.classList.add('event-message');
+                message.content = message.sender + ' joined!';
+            } else if (message.type === 'LEAVE') {
+                messageElement.classList.add('event-message');
+                message.content = message.sender + ' left!';
+            } else {
+                messageElement.classList.add('chat-message');
+
+                var avatarElement = document.createElement('i');
+                var avatarText = document.createTextNode(message.sender[0]);
+                avatarElement.appendChild(avatarText);
+                avatarElement.style['background-color'] = _this.getAvatarColor(message.sender);
+
+                messageElement.appendChild(avatarElement);
+
+                var usernameElement = document.createElement('span');
+                var usernameText = document.createTextNode(message.sender);
+                usernameElement.appendChild(usernameText);
+                messageElement.appendChild(usernameElement);
+            }
+
+            var textElement = document.createElement('p');
+            var messageText = document.createTextNode(message.content);
+            textElement.appendChild(messageText);
+
+            messageElement.appendChild(textElement);
+
+            document.querySelector('#messageArea').appendChild(messageElement);
+            document.querySelector('#messageArea').scrollTop = document.querySelector('#messageArea').scrollHeight;
+
+          });
+
+          _this.stompClient.send("/app/chat.addUser",{}, JSON.stringify({sender: this.username, type: 'JOIN'}))
+  
+          document.querySelector<HTMLDivElement>('.connecting').classList.add('hidden');
+        }, this.onError);
+    event.preventDefault();
     }
   }
- 
-  connect() {
-    const socket = new SockJS('https://applicationfitness.herokuapp.com/gkz-stomp-endpoint');
-    this.stompClient = Stomp.over(socket);
- 
-    const _this = this;
-    this.stompClient.connect({}, function (frame) {
-      _this.setConnected(true);
-      console.log('Connected: ' + frame);
- 
-      _this.stompClient.subscribe('/topic/hi', function (hello) {
-        _this.showGreeting(JSON.parse(hello.body).greeting);
-      });
-    });
-  }
- 
-  disconnect() {
-    if (this.stompClient != null) {
-      this.stompClient.disconnect();
-    }
- 
-    this.setConnected(false);
-    console.log('Disconnected!');
-  }
- 
-  sendName() {
-    this.stompClient.send(
-      '/gkz/hello',
-      {},
-      JSON.stringify({ 'name': this.name })
-    );
-  }
- 
-  showGreeting(message) {
-    this.greetings.push(message);
-  }
-
-
 
 }
