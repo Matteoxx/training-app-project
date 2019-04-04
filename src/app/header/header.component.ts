55bc92a1c2b847c61ffc881778fc6d945b654504
matteoxx <mateuszniziolek96@gmail.com>
2019-04-04 19:04:03 +0200

    userAvatar added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,37 +1,38 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { LoginService } from '../login.service';
-import { Subscription } from 'rxjs';
+
+interface LoginResponse {
+  body: boolean;
+  token: string;
+  username: string;
+  roles: string[];
+  photo: string;
+}
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit, OnDestroy {
-
-  loggedSubscription: Subscription;
+export class HeaderComponent implements OnInit {
 
   faUser = faUser;
-  
-  loggedIn = false;
-
   role = 'ROLE_GUEST';
+  userData: LoginResponse;
+  userAvatar: string;
+  username: string;
 
   constructor(private loginService: LoginService) { }
 
   ngOnInit() {
-    this.loggedSubscription = this.loginService.loggedIn.subscribe(
-      (loggedInStatus: boolean) => {
-        this.loggedIn = loggedInStatus;
-      }
-    );
-
+   
     this.loginService.role.subscribe(
       (role: string) => {
         this.role = role;
       }
     );
+
   }
 
   onLogout(){
@@ -40,8 +41,15 @@ export class HeaderComponent implements OnInit, OnDestroy {
     localStorage.removeItem('userData');
   }
 
-  ngOnDestroy(){
-    this.loggedSubscription.unsubscribe();
+  ngDoCheck(): void {
+    //Called every time that the input properties of a component or a directive are checked. Use it to extend change detection by performing a custom check.
+    //Add 'implements DoCheck' to the class.
+    if(this.role === 'ROLE_USER'){
+      this.userData = JSON.parse(localStorage.getItem('userData'));
+      this.userAvatar = this.userData.photo;
+      this.username = this.userData.username;
+    }
+    
   }
 
 }
